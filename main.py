c7808c705f74fe9f992bf9e22ecda922726fdf1b
ethiscold <156852615+ethiscold@users.noreply.github.com>
2026-02-21 10:25:32 -0500

    csv reading

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from national-bank-challenge!")
+import pandas as pd
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+@app.route("/")
+
+def load_csv(file_path):
+    try:
+        # Read CSV file
+        df = pd.read_csv(file_path, skiprows=1, header=0)
+        
+        # Display basic information
+        # timestamp,asset,side,quantity,entry_price,exit_price,profit_loss,balance
+        print("\nCSV Loaded Successfully!\n")
+        print("First 5 rows:")
+        print(df)
+        
+        print("\nDataset Info:")
+        print(df.info())
+        
+        print("\nSummary Statistics:")
+        print(df.describe())
+        return df
+
+    except FileNotFoundError:
+        print("File not found. " + file_path)
+    except pd.errors.EmptyDataError:
+        print("CSV file is empty.")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+
+def get_data(data):
+    return jsonify(data)
 
 if __name__ == "__main__":
-    main()
+    file_path = input("Enter path to your CSV file: ")
+    data = load_csv(file_path)
+
